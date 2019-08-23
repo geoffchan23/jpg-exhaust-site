8cab33fa1b91487537f037459349ac1f71da8e21
geoffchan23 <geoffchan23@gmail.com>
2019-08-23 12:09:11 -0400

    allow emails to have dots

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -49,7 +49,7 @@ class Home extends React.Component {
   }
   checkEmailValid = (email) => {
     this.setState({
-      emailValid: /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(email)
+      emailValid: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
     })
   }
   updateMessage = (e) => {
