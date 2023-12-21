a65d5df088687deb9c0448f534f0097f13386fc3
Geoff Chan <geoffchan23@gmail.com>
2023-12-21 10:36:15 -0500

    Update Home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -104,7 +104,7 @@ class Home extends React.Component {
         {
           !this.state.infoMessageClosed && (
             <div onClick={this.closeInfoMessage} className='info-message'>
-              JPG Exhaust will be closed from December 22, 2023 until January 1, 2023.
+              JPG Exhaust will be closed from December 22, 2023 until January 1, 2024.
               <button className='close'>
                 <MdClose />
               </button>
